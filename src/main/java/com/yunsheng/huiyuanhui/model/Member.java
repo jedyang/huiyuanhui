24f69fedc4923fd224a1d41a4bd39a7c7725f9a2
yunsheng <helloyunsheng@126.com>
2017-12-26 22:03:16 +0800

    up

---
+++ src/main/java/com/yunsheng/huiyuanhui/model/Member.java
@@ -0,0 +1,73 @@
+package com.yunsheng.huiyuanhui.model;
+
+public class Member {
+    private Integer memberId;
+
+    private Integer userId;
+
+    private String name;
+
+    private String phone;
+
+    private String passwd;
+
+    private Double money;
+
+    private Integer points;
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd == null ? null : passwd.trim();
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+}
---
+++ src/main/java/com/yunsheng/huiyuanhui/model/ShopUser.java
@@ -0,0 +1,65 @@
+package com.yunsheng.huiyuanhui.model;
+
+import java.util.Date;
+
+public class ShopUser {
+    private Integer userId;
+
+    private String passwd;
+
+    private String userName;
+
+    private String phone;
+
+    private Date createTime;
+
+    private Integer status;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd == null ? null : passwd.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
