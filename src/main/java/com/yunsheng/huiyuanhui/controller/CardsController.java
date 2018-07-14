b8e93de77f6e7a14f15a55b1fca8aec764ff827c
yunsheng <helloyunsheng@126.com>
2018-07-14 21:53:02 +0800

    消费记录

---
+++ src/main/java/com/yunsheng/huiyuanhui/controller/CardsController.java
@@ -0,0 +1,14 @@
+package com.yunsheng.huiyuanhui.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardsController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+}
